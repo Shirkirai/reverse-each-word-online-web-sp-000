005cdb5a821a8d9a255bd3bf20946724fec616bd
Jung Lee <lee.jung.s95@gmail.com>
2020-04-22 01:06:17 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,6 +2,6 @@ def reverse_each_word(sentence)
   array_sentence = sentence.split(" ")
   reversed_array_sentence = []
   array_sentence.each do |word|
-    reversed_array_sentence.push("#{word}.join(" ")")
+    puts reversed_array_sentence.push("#{word}.join(" ")")
   end
 end
